b00c58430e0bcf6e14c0e4271c1f33c22a72b020
ThalesIanMendes <thales.wow2@hotmail.com>
2022-07-22 12:06:27 -0300

    add cursor-pointer options preview

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,41 +5,41 @@ function Menu(){
         <>
             <section className="bg-neutral-800 h-16 border-t border-zinc-500 flex">
                 <div className="w-2/6 flex items-center gap-2">
-                    <div className="ml-1.5 w-9 ">
+                    <div className="ml-1.5 w-9 cursor-pointer">
                         <img src="./src/img/img-album.jpg" alt="" />
                     </div>
                     <div className="block text-white">
-                        <h3 className="text-sm">Ghost</h3>
-                        <h4 className="text-xs">Justin Bieber</h4>
+                        <h3 className="text-sm cursor-pointer">Ghost</h3>
+                        <h4 className="text-xs cursor-pointer">Justin Bieber</h4>
                     </div>
-                    <div className="text-white">
+                    <div className="text-white cursor-pointer">
                         <HeartStraight size={16} />
                     </div>
                 </div>
                 <div className="w-2/4 flex items-center">
                     <div className="block ml-12">
                         <div className="flex gap-4 ml-12 text-white">
-                            <Shuffle size={16} />
-                            <SkipBack size={16} />
-                            <PlayCircle size={16} />
-                            <SkipForward size={16} />
-                            <ArrowUDownLeft size={16} />
+                            <Shuffle className="cursor-pointer" size={16} />
+                            <SkipBack className="cursor-pointer" size={16} />
+                            <PlayCircle className="cursor-pointer" size={16} />
+                            <SkipForward className="cursor-pointer" size={16} />
+                            <ArrowUDownLeft className="cursor-pointer" size={16} />
                         </div>
                         <div className="flex">
                             <p>00:00</p>
-                            <p className="text-white"> ─────────────── </p>
+                            <p className="text-white cursor-pointer"> ─────────────── </p>
                             <p>03:20</p>
                         </div>
                     </div>
                 </div>
                 <div className="w-2/6 flex items-center">
                     <div className="flex ml-14 items-center text-white">
-                        <MicrophoneStage size={16} />
-                        <List size={16} />
-                        <DeviceMobileCamera size={16} />
-                        <SpeakerSimpleHigh size={16} />
-                        <p className="text-white items-center"> ─────── </p>
-                        <ArrowsOutSimple size={16} />
+                        <MicrophoneStage className="cursor-pointer" size={16} />
+                        <List className="cursor-pointer" size={16} />
+                        <DeviceMobileCamera className="cursor-pointer" size={16} />
+                        <SpeakerSimpleHigh className="cursor-pointer" size={16} />
+                        <p className="text-white items-center cursor-pointer"> ─────── </p>
+                        <ArrowsOutSimple className="cursor-pointer" size={16} />
                     </div>
                 </div>    
             </section>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,42 +5,42 @@ function Sidebar(){
         <>
             <div className="bg-black w-1/5 h-72">
                 <div className="mt-1 ml-2">
-                    <DotsThree className="text-white" size={16} />
+                    <DotsThree className="text-white cursor-pointer" size={16} />
                     <div className="flex items-center gap-3 text-xs mt-1">
                         <House size={16} />
-                        <p>Início</p>
+                        <p className="cursor-pointer">Início</p>
                     </div>
                     <div className="flex items-center gap-3 text-xs mt-1">
                         <MagnifyingGlass size={16} />
-                        <p>Buscar</p>
+                        <p className="cursor-pointer">Buscar</p>
                     </div>
                     <div className="flex items-center gap-3 text-xs mt-1">
                         <Books size={16} />
-                        <p>Sua Biblioteca</p>
+                        <p className="cursor-pointer">Sua Biblioteca</p>
                     </div>
                     <div className="flex items-center gap-3 text-xs mt-4">
                         <Plus className="bg-white text-black rounded-sm" size={16} />
-                        <p>Criar playlist</p>
+                        <p className="cursor-pointer">Criar playlist</p>
                     </div>
                     <div className="flex items-center gap-2 text-xs mt-2">
                         <HeartStraight className="bg-violet-500 text-white rounded-sm" size={16} />
-                        <p className="text-xs">Músicas Curtidas</p>
+                        <p className="text-xs cursor-pointer">Músicas Curtidas</p>
                         <ArrowCircleDown className="text-white" size={16} />
                     </div>
                     <div className=" mt-3 border-t text-slate-200 mr-2">
                     </div>
 
                     <div className="text-xs mt-2">
-                        <p>Sad Songs</p>
+                        <p className="cursor-pointer">Sad Songs</p>
                         <div className="flex gap-2 items-center">
-                            <p>This Is  The Earth</p>
+                            <p className="cursor-pointer">This Is  The Earth</p>
                             <ArrowCircleDown className="text-white" size={12} />
                         </div>
-                        <p>Descobertas da Semana</p>
-                        <p>Today's Top Hits</p>
-                        <p>Pop Up</p>
-                        <p>Hora Acústica</p>
-                        <p>Minha Playlist</p>
+                        <p className="cursor-pointer">Descobertas da Semana</p>
+                        <p className="cursor-pointer">Today's Top Hits</p>
+                        <p className="cursor-pointer">Pop Up</p>
+                        <p className="cursor-pointer">Hora Acústica</p>
+                        <p className="cursor-pointer">Minha Playlist</p>
                     </div>
                     
                 </div>
